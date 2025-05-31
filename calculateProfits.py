09e9a10acac08313727d90ebdd36834469a16964
nanact <144712705+nanact@users.noreply.github.com>
2025-05-31 16:11:42 -0200

    r

---
+++ calculateProfits.py
@@ -0,0 +1,15 @@
+def calculateProfits(arr,arr_size):
+    profit = 0
+    for i in range(1, arr_size):
+        
+        if arr[i] >arr[i-1]:
+            profit += arr[i] > arr[i]
+            
+            profit += arr[i] - arr[i-1]
+    return profit
+
+prices = [635,868,247,325,257,745,245]
+
+profit = calculateProfits(prices, len(prices))
+print("Max profit: ",profit)
+    
